fe1ab4c72ffa9bc2ac472f8e993bf4a0351423bd
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-03-22 22:32:46 +0700

    Initial commit

--- index.js
+++ index.js
@@ -18,38 +18,43 @@ const toMarkdown = (ast) => {
 };
 
 const mainDir = ".";
-let README = readdirSync(mainDir).includes("readme.md")
-  ? "readme.md"
-  : "README.md";
-const lang = core.getInput("LANG") || "zh-CN";
-const readme = readFileSync(join(mainDir, README), { encoding: "utf8" });
-const readmeAST = toAst(readme);
-console.log("AST CREATED AND READ");
+const lang = core.getInput("LANG") || "en";
+const mdFiles = core.getInput("FILES").split(/\r|\n/);
 
-let originalText = [];
+async function translate(files) {
+  for (const file of files) {
+    const readme = readFileSync(join(mainDir, file), { encoding: "utf8" });
+    const readmeAST = toAst(readme);
+    console.log(`${file} AST CREATED AND READ`);
 
-visit(readmeAST, async (node) => {
-  if (node.type === "text") {
-    originalText.push(node.value);
-    node.value = (await $(node.value, { to: lang })).text;
+    let originalText = [];
+
+    visit(readmeAST, async (node) => {
+      if (node.type === "text") {
+        originalText.push(node.value);
+        node.value = (await $(node.value, { to: lang })).text;
+      }
+    });
+
+    const translatedText = originalText.map(async (text) => {
+      return (await $(text, { to: lang })).text;
+    });
+
+    await writeToFile(file, readmeAST, translatedText);
   }
-});
+}
 
-const translatedText = originalText.map(async (text) => {
-  return (await $(text, { to: lang })).text;
-});
-
-async function writeToFile() {
+async function writeToFile(filename, readmeAST, translatedText) {
   await Promise.all(translatedText);
   writeFileSync(
-    join(mainDir, `README.${lang}.md`),
+    join(mainDir, `${filename}.${lang}.md`),
     toMarkdown(readmeAST),
     "utf8"
   );
-  console.log(`README.${lang}.md written`);
+  console.log(`${filename}.${lang}.md written`);
 }
 
-async function commitChanges(lang) {
+async function commitChanges() {
   console.log("commit started");
   await git.add("./*");
   await git.addConfig("user.name", "github-actions[bot]");
@@ -67,8 +72,8 @@ async function commitChanges(lang) {
 
 async function translateReadme() {
   try {
-    await writeToFile();
-    await commitChanges(lang);
+    await translate(mdFiles);
+    await commitChanges();
     console.log("Done");
   } catch (error) {
     throw new Error(error);
